ae087526c834a34edc2ddb235a345da3eb20e33a
GusthavoSoares <gusthavorsoares@gmail.com>
2023-06-18 12:54:06 -0300

    Adicionando o e-mail

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("Aplicação está se conectando ao banco de dados");
-//O Gusthavo fez esse commit no dia seguinte!
-console.log("Testando os comandos do git!");
 
 const paragrafo = document.querySelector('.cadastro-paragrafo');
 const botao = document.querySelector("#btn")
