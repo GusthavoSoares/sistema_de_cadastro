8a7f17270d396f2e8299624d1144534857a218bc
GusthavoSoares <gusthavorsoares@gmail.com>
2023-06-18 11:27:40 -0300

    Modificando a estrutura do projeto

--- app.js
+++
@@ -1,5 +0,0 @@
-console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("Aplicação está se conectando ao banco de dados");
-//O Gusthavo fez esse commit no dia seguinte!
-console.log("Testando os comandos do git!");
-
---
+++ js/app.js
@@ -0,0 +1,27 @@
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação está se conectando ao banco de dados");
+//O Gusthavo fez esse commit no dia seguinte!
+console.log("Testando os comandos do git!");
+
+const paragrafo = document.querySelector('.cadastro-paragrafo');
+const botao = document.querySelector("#btn")
+const input = document.querySelector('input[type=text]');
+
+const form = document.querySelector("form");
+
+
+
+form.addEventListener("submit", (event)=>
+{
+    event.preventDefault();
+})
+
+botao.addEventListener("click", (evento)=>{
+    escreverTexto(paragrafo, input.value)
+})
+
+function escreverTexto(elemento, texto){
+    elemento.innerText = texto;
+}
+
+
